634f286a9309211048ff709c288f5e52616d4d45
tttnishimo <tttnishimo@gmail.com>
2020-03-11 15:36:24 +0900

    [AC] abc039 abc039_c

---
+++ atcoder.jp/abc039/abc039_c/Main.py
@@ -0,0 +1,15 @@
+s = input()
+if s[:12] == "WBWBWWBWBWBW":
+  print("Do")
+elif s[:12] == "WBWWBWBWBWWB":
+  print("Re")
+elif s[:12] == "WWBWBWBWWBWB":
+  print("Mi")
+elif s[:12] == "WBWBWBWWBWBW":
+  print("Fa")
+elif s[:12] == "WBWBWWBWBWWB":
+  print("So")
+elif s[:12] == "WBWWBWBWWBWB":
+  print("La")
+elif s[:12] == "WWBWBWWBWBWB":
+  print("Si")
