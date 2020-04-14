be1c4b1374726b007fb3030b1bd6409c307c77f6
tttnishimo <tttnishimo@gmail.com>
2020-04-15 00:07:44 +0900

    [AC] abc021 abc021_a

---
+++ atcoder.jp/abc021/abc021_a/Main.py
@@ -0,0 +1,38 @@
+n = int(input())
+if n == 1:
+  print(1)
+  print(1)
+elif n == 2:
+  print(1)
+  print(2)
+elif n == 3:
+  print(2)
+  print(1)
+  print(2)
+elif n == 4:
+  print(1)
+  print(4)
+elif n == 5:
+  print(2)
+  print(1)
+  print(4)
+elif n == 6:
+  print(2)
+  print(2)
+  print(4)
+elif n == 7:
+  print(3)
+  print(1)
+  print(2)
+  print(4)
+elif n == 8:
+  print(1)
+  print(8)
+elif n == 9:
+  print(2)
+  print(1)
+  print(8)
+else:
+  print(2)
+  print(2)
+  print(8)
