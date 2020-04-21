789073388bbce74ea2f5f2a7da8b6a63692979a3
tttnishimo <tttnishimo@gmail.com>
2020-04-21 10:26:39 +0900

    [AC] abc033 abc033_a

---
+++ atcoder.jp/abc033/abc033_a/Main.py
@@ -0,0 +1,5 @@
+s = input()
+if s == '0000' or s == '1111' or s == '2222' or s == '3333' or s == '4444' or s == '5555' or s == '6666' or s == '7777' or s == '8888' or s == '9999':
+  print('SAME')
+else:
+  print('DIFFERENT')
