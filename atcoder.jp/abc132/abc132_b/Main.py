1d124288aa5c7702d6867ab128bdb8a3bf9c8cdf
tttnishimo <tttnishimo@gmail.com>
2020-04-26 17:00:09 +0900

    [AC] abc132 abc132_b

---
+++ atcoder.jp/abc132/abc132_b/Main.py
@@ -0,0 +1,7 @@
+n = int(input())
+a = list(map(int, input().split()))
+res = 0
+for i in range(n-2):
+  if (a[i] > a[i+1] and a[i+1] > a[i+2]) or (a[i] < a[i+1] and a[i+1] < a[i+2]):
+    res += 1
+print(res)
