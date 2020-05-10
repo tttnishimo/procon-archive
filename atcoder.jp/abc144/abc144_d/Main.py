7c09684ff4383097bf76f6932f7942326e4cb900
tttnishimo <tttnishimo@gmail.com>
2020-05-10 15:25:09 +0900

    [AC] abc144 abc144_d

---
+++ atcoder.jp/abc144/abc144_d/Main.py
@@ -0,0 +1,8 @@
+import math
+a,b,x = map(int, input().split())
+if x == a*a*b:
+  print(0)
+elif x >= a*a*b/2:
+  print(math.atan((2*a*a*b-2*x)/a/a/a)*180/math.pi)
+else:
+  print(math.atan(a*b*b/2/x)*180/math.pi)
