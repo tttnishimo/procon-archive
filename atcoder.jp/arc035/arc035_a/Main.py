462a9711722f5b8955e4e6bc189a068625078a45
tttnishimo <tttnishimo@gmail.com>
2020-05-14 22:06:44 +0900

    [AC] arc035 arc035_a

---
+++ atcoder.jp/arc035/arc035_a/Main.py
@@ -0,0 +1,11 @@
+s = input()
+flg = 0
+for i in range(len(s)//2):
+  if s[i] == s[-i-1] or s[i] == '*' or s[-i-1] == '*':
+    pass
+  else:
+    flg = 1
+if flg == 0:
+  print('YES')
+else:
+  print('NO')
