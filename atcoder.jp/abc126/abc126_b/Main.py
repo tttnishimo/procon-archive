c598640dd3d680473d3f0bb96e573c696f85bbee
tttnishimo <tttnishimo@gmail.com>
2020-05-20 21:00:53 +0900

    [AC] abc126 abc126_b

---
+++ atcoder.jp/abc126/abc126_b/Main.py
@@ -0,0 +1,9 @@
+s=input()
+if 1 <= int(s[2:]) <= 12 and 1 <= int(s[:2]) <= 12:
+  print('AMBIGUOUS')
+elif 1 <= int(s[2:]) <= 12:
+  print('YYMM')
+elif 1 <= int(s[:2]) <= 12:
+  print('MMYY')
+else:
+  print('NA')
