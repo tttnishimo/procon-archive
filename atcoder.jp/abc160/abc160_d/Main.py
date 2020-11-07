774011d845d5970a0b9ff38dda360a30f3adacf2
tttnishimo <tttnishimo@gmail.com>
2020-11-07 11:34:54 +0900

    [AC] abc160 abc160_d

---
+++ atcoder.jp/abc160/abc160_d/Main.py
@@ -0,0 +1,6 @@
+n,x,y=map(int, input().split())
+ans=[0]*n
+for i in range(1,n):
+  for j in range(i+1,n+1):
+    ans[min(abs(j-i),abs(x-i)+1+abs(y-j),abs(y-i)+1+abs(x-j))]+=1
+print(*ans[1:],sep='\n')
