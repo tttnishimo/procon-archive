2705622910ed3a46e572f0ce177cb9fe65c1569a
tttnishimo <tttnishimo@gmail.com>
2021-08-04 08:46:44 +0900

    [AC] abc212 abc212_b

---
+++ atcoder.jp/abc212/abc212_b/Main.py
@@ -0,0 +1,5 @@
+s=input()
+if s=='0000' or s=='1111' or s=='2222' or s=='3333' or s=='4444' or s=='5555' or s=='6666' or s=='7777' or s=='8888' or s=='9999' or s=='0123' or s=='1234' or s=='2345' or s=='3456' or s=='4567' or s=='5678' or s=='6789' or s=='7890' or s=='8901' or s=='9012':
+  print('Weak')
+else:
+  print('Strong')
