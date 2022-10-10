ae9c8e06a7a62291a521848357afa49d1efe96e5
tttnishimo <tttnishimo@gmail.com>
2022-10-10 15:29:38 +0900

    [AC] APG4b APG4b_ci

---
+++ atcoder.jp/APG4b/APG4b_ci/Main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  vector<int> vec(3);
+  cin >> vec.at(0) >> vec.at(1) >> vec.at(2);
+  sort(vec.begin(), vec.end());
+  cout << vec.at(2) - vec.at(0) << endl;
+}
